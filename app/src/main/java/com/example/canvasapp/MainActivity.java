f4792a39f319ee99e38c3ab2267821d996931ef1
Antonius Wisnu <antonius.kri2003@mail.ugm.ac.id>
2023-05-03 15:59:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/canvasapp/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.canvasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Canvas mCanvas;
+    private Paint mPaint = new Paint();
+    private Paint mPaintText = new Paint(Paint.UNDERLINE_TEXT_FLAG);
+    private Bitmap mBitmap;
+    private ImageView mImageView;
+
+    private Rect mRect = new Rect();
+    private Rect mBounds = new Rect();
+    private static final int OFFSET = 12;
+    private int mOffset = OFFSET;
+    private static final int MULTIPLIER = 100;
+
+    private int mColorBackground ;
+    private int mColorRectangle;
+    private int mColorCircle;
+    private int mColorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mColorBackground = ResourcesCompat.getColor(getResources(), R.color.colorBackground, null);
+        mColorRectangle = ResourcesCompat.getColor(getResources(), R.color.colorRectangle, null);
+        mColorCircle = ResourcesCompat.getColor(getResources(), R.color.colorAccent, null);
+        mColorText = ResourcesCompat.getColor(getResources(), R.color.black, null);
+
+        mPaint.setColor(mColorBackground);
+        mPaint.setColor(mColorText);
+        mPaint.setTextSize(20);
+
+        mImageView = findViewById(R.id.my_image_view);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawSomething(view);
+            }
+        });
+    }
+
+    public void drawSomething(View view){
+        int mWidth = view.getWidth();
+        int mHeight = view.getHeight();
+
+        int halfWidth = mWidth/2;
+        int halfHeight = mHeight/2;
+
+        if (mOffset == OFFSET){
+            mBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
+            mImageView.setImageBitmap(mBitmap);
+            mCanvas = new Canvas(mBitmap);
+            mCanvas.drawColor(mColorBackground);
+
+            mCanvas.drawText(getString(R.string.keep_tapping), 100, 100, mPaintText);
+            mOffset += OFFSET;
+        } else {
+
+            if(mOffset < halfWidth && mOffset < halfHeight){
+                mPaint.setColor(mColorRectangle - MULTIPLIER * mOffset);
+                mRect.set(mOffset, mOffset, mWidth - mOffset, mHeight - mOffset);
+                mCanvas.drawRect(mRect , mPaint);
+                mOffset += OFFSET;
+            } else {
+                mPaint.setColor(mColorCircle - MULTIPLIER * mOffset);
+                mCanvas.drawCircle(halfWidth, halfHeight, halfWidth/5, mPaint);
+                mOffset += OFFSET;
+
+                String text = getString(R.string.done);
+
+                mPaintText.getTextBounds(text, 0, text.length(), mBounds);
+                mPaintText.setTextSize(100);
+
+                // Text biar di tengah
+                int x = halfWidth - mBounds.centerX();
+                int y = halfHeight - mBounds.centerY();
+
+                mCanvas.drawText(text, x,y, mPaintText);
+
+                mPaint.setColor(mColorBackground - MULTIPLIER * mOffset);
+                Point a = new Point(halfWidth-50, halfHeight-50);
+                Point b = new Point(halfWidth+50, halfHeight+50);
+                Point c = new Point(halfWidth-50, halfHeight+50);
+
+
+                Path path = new Path();
+                path.setFillType(Path.FillType.EVEN_ODD);
+                path.lineTo(a.x, a.y);
+                path.lineTo(b.x, b.y);
+                path.lineTo(c.x, c.y);
+                path.lineTo(a.x, a.y);
+                path.close();
+
+                mCanvas.drawPath(path, mPaint);
+                mOffset += OFFSET;
+            }
+
+        }
+
+        view.invalidate();
+
+    }
+}
